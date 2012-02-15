584bbcb566bd7f91e265746117fba19bf03b4d25
Clément SIPIETER <c.sipieter@gmail.com>
2012-02-15 19:04:29 +0100

    Add licence informations

--- include/lstr.h
+++ include/lstr.h
@@ -1,8 +1,20 @@
-/* 
- * File:   Str.h
- * Author: clement
+/*  
+ *  Copyright © 2011 Sipieter Clément <csipieter@gmail.com>
  *
- * Created on 25 septembre 2010, 17:55
+ *  This file is part of nlp-morpho.
+ *
+ *  nlp-morpho is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  nlp-morpho is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with nlp-morpho.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef LSTR_H
--- src/Rule.cpp
+++ src/Rule.cpp
@@ -1,3 +1,22 @@
+/*  
+ *  Copyright © 2011 Sipieter Clément <csipieter@gmail.com>
+ *
+ *  This file is part of nlp-morpho.
+ *
+ *  nlp-morpho is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  nlp-morpho is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with nlp-morpho.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "Rule.h"
 
 Rule::Rule(const string& str_rule)
